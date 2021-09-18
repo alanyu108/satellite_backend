4034909ad89222e252cc65a53ff3385a67f5637e
alanyu108 <alanwong103@gmail.com>
2021-09-18 16:02:11 -0400

    CHANGES
    -changed satellite model to use tle data -added CORS

--- satellite/models.py
+++ satellite/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from django.db.models.fields import DecimalField
 
 # Create your models here.
 class Satellite(models.Model):
     name = models.CharField(max_length=24)
-    long = models.DecimalField(max_digits=5,decimal_places=2, null=False, blank=False)
-    lat = models.DecimalField(max_digits=5,decimal_places=2, null=False, blank=False)
-    active = models.BooleanField(default=False)
+    tle_1 = models.CharField(max_length=80, blank=False, null=False)
+    tle_2 = models.CharField(max_length=80, blank=False, null=False)
--- satellite_backend/settings.py
+++ satellite_backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #3rd party apps
+    'corsheaders',
     'rest_framework',
     #own apps
     'satellite.apps.SatelliteConfig',
@@ -53,12 +54,20 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+"""
+ONLY IN PROD OTHERWISE USE
+CORS_ALLOW_ORIGINS = []
+"""
+CORS_ORIGIN_ALLOW_ALL = True;
+
 
 ROOT_URLCONF = 'satellite_backend.urls'
 
