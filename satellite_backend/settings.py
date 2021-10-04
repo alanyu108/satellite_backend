47bc2eb869b7c3e44821dd768ea58a32c0caad85
alanyu108 <alanwong103@gmail.com>
2021-10-03 21:24:17 -0400

    changed settings.py
    set debug in settings.py to true in dev and false in prod

--- satellite_backend/settings.py
+++ satellite_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = [
     'alanyu108-satellite-backend.herokuapp.com',
